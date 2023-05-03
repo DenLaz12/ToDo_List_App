a8df6e38e0d78bfe6ff83397e4801b64314c8159
Denys <denislazarenko12@gmail.com>
2023-05-03 18:55:15 +0200

    first commit

---
+++ src/main/java/denyslazarenko/com/todo_list_app/ToDoListAppApplication.java
@@ -0,0 +1,14 @@
+package denyslazarenko.com.todo_list_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("sgdhf");
+        SpringApplication.run(ToDoListAppApplication.class, args);
+    }
+
+}
